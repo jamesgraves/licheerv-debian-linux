e5046c26367a703dc288ac0093a3a64c9bcf3088
James Graves <james.c.graves.jr@gmail.com>
2022-03-24 11:55:03 -0400

    Fix build script, add ToC for u-boot
    The build script had an errant back quote '`' in it.
    
    Also added some echos to show what stage the build process
    has gotten to.
    
    Also detect if the toolchain has already been build so that
    can be skipped to save time.

--- build.sh
+++ build.sh
@@ -2,43 +2,52 @@
 
 set -eou pipefail
 
+echo "Updating all git submodules"
 git submodule update --init --recursive
 
 cwd=`pwd`
 
-echo "Build RISC-V toolchain"
-pushd riscv-gnu-toolchain
-./configure --prefix=$cwd/riscv64-unknown-linux-gnu --with-arch=rv64gc --with-abi=lp64d
-make linux -j `nproc`
-export PATH=$cwd/riscv64-unknown-linux-gnu/bin:$PATH
-popd
+if ! [ -d riscv64-unknown-linux-gnu -a -x riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu-gcc ]; then
+	echo "Build RISC-V toolchain"
+	pushd riscv-gnu-toolchain
+	./configure --prefix=$cwd/riscv64-unknown-linux-gnu --with-arch=rv64gc --with-abi=lp64d
+	make linux -j `nproc`
+	popd
+else
+	echo "RISC-V toolchain has been built."
+fi
 
-echo "Boot0 binary"
+export PATH=$cwd/riscv64-unknown-linux-gnu/bin:$PATH
+
+echo "Build boot0 binary"
 pushd sun20i_d1_spl
 make CROSS_COMPILE=$cwd/riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu- p=sun20iw1p1 mmc
 popd
 
-echo "OpenSBI binary"
+echo "Build OpenSBI binary"
 pushd opensbi
 make CROSS_COMPILE=$cwd/riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu- PLATFORM=generic FW_PIC=y FW_OPTIONS=0x2
 popd
 
-echo "u-boot binary"
+echo "Build u-boot binary"
+pushd u-boot
+make CROSS_COMPILE=$cwd/riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu- nezha_defconfig
 make CROSS_COMPILE=$cwd/riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu- nezha_defconfig
 make -j `nproc` ARCH=riscv CROSS_COMPILE=$cwd/riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu- all V=1
+popd
 
 echo "Generate u-boot table of contents"
-./u-boot/tools/mkimage -T sunxi_toc1 -d licheerv_toc1.cfg u-boot.toc1`
+./u-boot/tools/mkimage -T sunxi_toc1 -d licheerv_toc1.cfg u-boot.toc1
 
-echo "Linux kernel"
+echo "Build Linux kernel"
 mkdir -p linux-build/arch/riscv/configs
 cp licheerv_linux_defconfig linux-build/arch/riscv/configs/licheerv_defconfig
 make ARCH=riscv -C linux O=$cwd/linux-build licheerv_defconfig
 make -j `nproc` -C linux-build ARCH=riscv CROSS_COMPILE=$cwd/riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu- V=1
 
-echo "Realtek 8723DS WiFi Driver"
+echo "Build Realtek 8723DS WiFi Driver"
 pushd rtl8723ds
-make -j `nproc` ARCH=riscv CROSS_COMPILE=$cwd/riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu- KSRC=$cwd/linux-build modules`
+make -j `nproc` ARCH=riscv CROSS_COMPILE=$cwd/riscv64-unknown-linux-gnu/bin/riscv64-unknown-linux-gnu- KSRC=$cwd/linux-build modules
 popd
 
 echo "Successfully finished build process"
