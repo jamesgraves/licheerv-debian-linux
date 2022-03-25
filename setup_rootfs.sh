aede2e29b305a2ee2523b11c736d94a8d7927b7e
James Graves <james.c.graves.jr@gmail.com>
2022-03-24 20:23:03 -0400

    Fixed debootstrap, option to ignore gpg sig
    On at least Ubuntu bionic, the Debian GPG keys are out of date.

--- setup_rootfs.sh
+++ setup_rootfs.sh
@@ -3,26 +3,24 @@
 set -eou pipefail
 
 read -p "Type in WLAN SSID: " WLAN_SSID
-read -p "Type in WLAN secret key: " WLAN_SECRET
+read -s -p "Type in WLAN secret key: " WLAN_SECRET
+echo ""
 
-sudo debootstrap \
-	--arch=riscv64 \
-	--keyring /usr/share/keyrings/debian-ports-archive-keyring.gpg \
-	--components main,contrib,non-free \
-	--include=debian-ports-archive-keyring, \
-	pciutils, autoconf, automake, autotools-dev, curl, \
-	python3, libmpc-dev, libmpfr-dev, libgmp-dev, gawk, \
-	build-essential, bison, flex, texinfo, gperf, libtool, \
-	patchutils, bc, zlib1g-dev, wpasupplicant, htop, net-tools, \
-	wireless-tools, firmware-realtek, ntpdate, openssh-client, \
-	openssh-server, sudo, e2fsprogs, git, man-db, lshw, dbus, \
-	wireless-regdb, libsensors5, lm-sensors, swig, libssl-dev, \
-	python3-distutils, python3-dev, alien, fakeroot, dkms, \
-	libblkid-dev, uuid-dev, libudev-dev, libaio-dev, libattr1-dev, \
-	libelf-dev, python3-setuptools, python3-cffi, python3-packaging, \
-	libffi-dev, libcurl4-openssl-dev, python3-ply, iotop, tmux, psmisc \
-	unstable \
-	rootfs http://deb.debian.org/debian-ports
+keyring_option="--keyring /usr/share/keyrings/debian-ports-archive-keyring.gpg"
+if [ $# -eq 1 ]; then
+	if [ $1 = "--no-check-gpg" ]; then
+		keyring_option="--no-check-gpg"
+	fi
+fi
+
+if sudo debootstrap --arch=riscv64 ${keyring_option} --components main,contrib,non-free --include=debian-ports-archive-keyring,pciutils,autoconf,automake,autotools-dev,curl,python3,libmpc-dev,libmpfr-dev,libgmp-dev,gawk,build-essential,bison,flex,texinfo,gperf,libtool,patchutils,bc,zlib1g-dev,wpasupplicant,htop,net-tools,wireless-tools,firmware-realtek,ntpdate,openssh-client,openssh-server,sudo,e2fsprogs,git,man-db,lshw,dbus,wireless-regdb,libsensors5,lm-sensors,swig,libssl-dev,python3-distutils,python3-dev,alien,fakeroot,dkms,libblkid-dev,uuid-dev,libudev-dev,libaio-dev,libattr1-dev,libelf-dev,python3-setuptools,python3-cffi,python3-packaging,libffi-dev,libcurl4-openssl-dev,python3-ply,iotop,tmux,psmisc unstable rootfs http://deb.debian.org/debian-ports
+then
+	echo "Created rootfs"
+else
+	echo "Failed to create rootfs using debootstrap."
+	echo "If the error is that the keyring is missing or out-of-date,"
+	echo "this command can be re-run with the --no-check-gpg option."
+fi
 
 pushd linux-build
 sudo make modules_install ARCH=riscv INSTALL_MOD_PATH=../rootfs KERNELRELEASE=5.17.0-rc2-379425-g06b026a8b714
