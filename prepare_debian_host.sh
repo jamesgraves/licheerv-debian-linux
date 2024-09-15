0b9218b20910b4713f55e82e223b718b622f5e8e
Daryl Van Dyke <GeospatialDaryl@users.noreply.github.com>
2024-09-14 21:54:48 -0700

    Update prepare_debian_host.sh
    remove python3-distutils

--- prepare_debian_host.sh
+++ prepare_debian_host.sh
@@ -6,6 +6,6 @@ sudo apt install \
 	autoconf automake autotools-dev curl python3 libmpc-dev \
 	libmpfr-dev libgmp-dev gawk build-essential bison flex texinfo \
 	gperf libtool patchutils bc zlib1g-dev libexpat-dev swig \
-	libssl-dev python3-distutils python3-dev \
+	libssl-dev  python3-dev \
 	debootstrap debian-ports-archive-keyring \
 	qemu-user-static qemu-system qemu-utils qemu-system-misc binfmt-support
